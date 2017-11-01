5483dba98cf576089270956409ddcfdb7ce58bb0
Maggie Weinroth <weinroth@rams.colostate.edu>
2017-11-01 13:14:25 -0600

    updated

--- R/Maggiesnotes.R
+++ R/Maggiesnotes.R
@@ -4,6 +4,8 @@ library(ggplot2)
 library(ggfortify)
 library(devtools)
 library(dlnm)
+library(readxl)
+library(tidyverse)
 
 cmax <- read_excel("../data/DRUG_CLASS_I_Mean_Cmax_Trough_Efficacy_R_DATA_ANALYSIS.xlsx", 
                    sheet = "Mean_PK_Efficacy_In vitro", skip = 1, n_max = 12)
@@ -14,9 +16,27 @@ trough <- read_excel("../data/DRUG_CLASS_I_Mean_Cmax_Trough_Efficacy_R_DATA_ANAL
 cmax_clean <- cmax %>%
   rename(drug = X__1)
 head(cmax_clean)
-cmax_long <- gather(cmax_clean, key = "ELU", value = "interest")
-hist(cmax_clean$PLA)
-#head_long <- lm(ELU ~ lng)
+cmax_long <- gather(cmax_clean, key = outcome, value = value_outcome, ELU:ESP)
+cmax_ELU <- cmax_long %>% 
+  filter(outcome == "ELU")
+cmax_ELU <- gather(cmax_ELU, key = var, value = value_var, PLA:SLE) 
+
+cmax_ELU <- cmax_ELU %>% 
+  select(outcome:value_var) %>% 
+  mutate(value_var = as.numeric(value_var)) %>% 
+  mutate(value_outcome = as.numeric(value_outcome)) %>% 
+  arrange(desc(value_outcome))
+
+
+
+ggplot(cmax_ELU, aes(x = value_outcome, y = value_var)) +
+  geom_point() +
+  facet_wrap(~var)
+
+library(stats)
+qqnorm(cmax_ELU$cLogP)
+
+mod1 <- lm(cmax_ELU, value_outcome ~ PLA:SLE)
 
 ##########################
 #7.6.1 Using regression models to explore data #1
